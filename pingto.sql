64263a36cb85d04866e4857dcd7cb84a3ac6df39
uberkie <44478849+uberkie@users.noreply.github.com>
2022-05-18 15:17:07 +0200

    Create pingto.sql

---
+++ pingto.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `pingto` (
+  `id` int(11) NOT NULL,
+  `ip_add` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
